2fc248d4269cf54743e9cbb44503b857a8830d91
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-07-10 19:57:57 +0200

    Add simple input form for welcome page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let username = $state('user_' + Math.floor(Math.random() * 10000));
+
+    function join_lobby() {
+        console.log('User choose to join an exisiting lobby')
+    }
+
+    function create_lobby() {
+        console.log('User choose to create a new lobby')
+    }
+</script>
+
+<h1>Play Mafia Online!</h1>
+
+<div id="container">
+  <div id="top">
+    <input type="text" bind:value={username} />
+  </div>
+  <div id='bottom'>
+    <button onclick={join_lobby}>Join lobby</button>
+    <button onclick={create_lobby}>Create lobby</button>
+  </div>
+</div>
+
+<style>
+</style>
