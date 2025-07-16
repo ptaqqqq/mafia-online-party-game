8fd37b02b8f9677eba7d4ce585afc5622e7e41e0
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-07-16 21:21:48 +0200

    Create GameState object

---
+++ backend/app/domain/game_state.py
@@ -0,0 +1,95 @@
+from enum import Enum
+from typing import Dict, Optional, TypedDict
+
+
+class Phase(Enum):
+    DAY = "day"
+    NIGHT = "night"
+    VOTING = "voting"
+    ENDED = "ended"
+
+
+class Role(Enum):
+    INNOCENT = "innocent"
+    MAFIA = "mafia"
+
+
+class GameWinner(Enum):
+    INNOCENT = "innocent"
+    MAFIA = "mafia"
+    DRAW = "draw"
+
+
+class PlayerState(TypedDict):
+    role: Role
+    alive: bool
+
+
+class InvalidPhaseError(Exception):
+    pass
+
+
+class GameState:
+    def __init__(self):
+        self.phase: Phase = Phase.NIGHT
+        self.players: Dict[str, PlayerState] = {}
+
+    def add_player(self, uuid: str, player_state: PlayerState):
+        self.players[uuid] = player_state
+
+    def remove_player(self, uuid: str):
+        del self.players[uuid]
+
+    def end_day(self):
+        if self.phase != Phase.DAY:
+            raise InvalidPhaseError
+
+        self.phase = Phase.VOTING
+
+    def end_voting(self, suspected_player_id: Optional[str] = None):
+        if self.phase != Phase.VOTING:
+            raise InvalidPhaseError
+
+        if suspected_player_id is not None:
+            if self.players.get(suspected_player_id, None) is None:
+                raise ValueError("Player {suspected_player} not found!")
+            self.players[suspected_player_id]["alive"] = False
+
+        if self.check_game_over() is not None:
+            self.phase = Phase.ENDED
+        else:
+            self.phase = Phase.NIGHT
+
+    def end_night(self, targeted_player_id: Optional[str] = None):
+        if self.phase != Phase.NIGHT:
+            raise InvalidPhaseError
+
+        if targeted_player_id is not None:
+            if self.players.get(targeted_player_id, None) is None:
+                raise ValueError("Player {suspected_player} not found!")
+            self.players[targeted_player_id]["alive"] = False
+
+        if self.check_game_over() is not None:
+            self.phase = Phase.ENDED
+        else:
+            self.phase = Phase.DAY
+
+    def check_game_over(self):
+        mafia = sum(
+            1 for p in self.players.values() if p["role"] == Role.MAFIA and p["alive"]
+        )
+        innocents = sum(
+            1
+            for p in self.players.values()
+            if p["role"] == Role.INNOCENT and p["alive"]
+        )
+
+        if mafia == 0:
+            if innocents == 0:
+                return GameWinner.DRAW
+            else:
+                return GameWinner.INNOCENT
+        elif mafia >= innocents:
+            return GameWinner.MAFIA
+        else:
+            return None
