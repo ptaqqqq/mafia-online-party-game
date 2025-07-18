4e53fc34206a108ec6a4a7bde86a4b252bd71af7
ptaqqqq <61513585+ptaqqqq@users.noreply.github.com>
2025-07-18 22:30:38 +0200

    Rewrite websocket.py to use GameManager

--- backend/app/routers/websocket.py
+++ backend/app/routers/websocket.py
@@ -1,175 +1,95 @@
 from collections import defaultdict
-from typing import Annotated, Dict, Set, Union
+import logging
+from typing import Dict, override
+from uuid import uuid4
+from app.domain.game_manager import GameManager, PlayerAdapter
 from fastapi import WebSocket, WebSocketDisconnect
 from fastapi.routing import APIRouter
-from fastapi_camelcase import CamelModel
-from pydantic import Field, ValidationError
+from pydantic import ValidationError
 
 
 from schemas.game import (
     ActionAck,
     ActionAckPayload,
-    GameStateSync,
-    MessageReceived,
-    MorningNews,
+    GameEvent,
     NightAction,
-    PhaseChange,
     PlayerJoin,
-    PlayerJoined,
-    PlayerJoinedPayload,
     PlayerLeave,
     PlayerLeavePayload,
-    PlayerLeft,
-    PlayerLeftPayload,
     SendMessage,
     Vote,
-    VoteCast,
-    VoteCastPayload,
-    VotePayload,
 )
-
-IncomingEvent = Annotated[
-    Union[PlayerJoin, PlayerLeave, NightAction, Vote, SendMessage],
-    Field(discriminator="type"),
-]
-
-OutgoingEvent = (
-    PlayerJoined
-    | PlayerLeft
-    | ActionAck
-    | MorningNews
-    | VoteCast
-    | PhaseChange
-    | MessageReceived
-    | GameStateSync
-)
-
-
-class WSIncomingMessage(CamelModel):
-    message: IncomingEvent = Field(..., discriminator="type")
 
 
 router = APIRouter()
 
 
-class ConnectionManager:
-    def __init__(self):
-        # defaultdict auto creates new rooms
-        self.rooms: Dict[str, Set[WebSocket]] = defaultdict(set)
+class WebSocketPlayerAdapter(PlayerAdapter):
+    def __init__(self, ws: WebSocket):
+        self.ws = ws
 
-    async def connect(self, ws: WebSocket, room_id: str):
-        await ws.accept()
-        self.rooms[room_id].add(ws)
-
-    def disconnect(self, ws: WebSocket, room_id: str):
-        self.rooms[room_id].remove(ws)
-        if not self.rooms[room_id]:
-            del self.rooms[room_id]
-
-    async def broadcast(self, room_id: str, event: OutgoingEvent):
-        for ws in self.rooms.get(room_id, []):
-            await ws.send_json(event.model_dump())
+    @override
+    async def receive_event(self, event: GameEvent):
+        logging.info(f"Sending event {event.model_dump_json()}")
+        await self.ws.send_json(event.model_dump())
 
 
-manager = ConnectionManager()
+room_game_managers: Dict[str, GameManager] = defaultdict(GameManager)
 
-# maps websocket to uuid
-player_map: Dict[WebSocket, str] = {}
+# maps uuid to adapter
+uuid_adapters: Dict[str, WebSocketPlayerAdapter] = {}
 
 
 @router.websocket("/{room_id}")
 async def websocket_endpoint(ws: WebSocket, room_id: str):
-    await manager.connect(ws, room_id)
+    await ws.accept()
+    ws_uuid = str(uuid4())
+    ws_adapter = WebSocketPlayerAdapter(ws)
+    uuid_adapters[ws_uuid] = ws_adapter
+
+    await ws.send_json({"type": "player.uuid", "payload": {"uuid": ws_uuid}})
+
     try:
         while True:
-            msg = await ws.receive_json()
-            wrapper = WSIncomingMessage.model_validate(msg)
-            event = wrapper.message
+            try:
+                msg = await ws.receive_json()
 
-            # TODO: refactor
-            if isinstance(event, PlayerJoin):
-                pj = event
-                player_map[ws] = pj.payload.player_id
-                await manager.broadcast(
-                    room_id,
-                    PlayerJoined(
-                        type="player.joined",
-                        payload=PlayerJoinedPayload(
-                            player_id=pj.payload.player_id, name=pj.payload.name
-                        ),
-                    ),
-                )
-            else:
-                if player_map.get(ws, None) is None:
-                    await ws.send_json(
-                        ActionAck(
-                            type="action.ack",
-                            payload=ActionAckPayload(
-                                success=False,
-                                message="Must join before performing any actions.",
-                            ),
-                        ).model_dump()
-                    )
-                    continue
-
-                match event:
-                    case PlayerLeave(payload=PlayerLeavePayload(player_id=player_id)):
-                        await manager.broadcast(
-                            room_id,
-                            PlayerLeft(
-                                type="player.left",
-                                payload=PlayerLeftPayload(player_id=player_id),
-                            ),
-                        )
-                    case NightAction(payload=payload):
-                        # TODO: gameplay logic
-                        pass
-                    case Vote(
-                        payload=VotePayload(actor_id=actor_id, target_id=target_id)
-                    ):
-                        # TODO: gameplay logic + validate if possible
-                        await manager.broadcast(
-                            room_id,
-                            VoteCast(
-                                type="action.vote_cast",
-                                payload=VoteCastPayload(
-                                    actor_id=actor_id, target_id=target_id
-                                ),
-                            ),
-                        )
-                    case SendMessage(payload=payload):
-                        # TODO: gameplay logic + validate if possible
-                        await manager.broadcast(
-                            room_id,
-                            MessageReceived(type="message.received", payload=payload),
-                        )
+                match msg["type"]:
+                    case "player.join":
+                        event = PlayerJoin.model_validate(msg)
+                    case "player.leave":
+                        event = PlayerLeave.model_validate(msg)
+                    case "action.night":
+                        event = NightAction.model_validate(msg)
+                    case "action.vote":
+                        event = Vote.model_validate(msg)
+                    case "message.send":
+                        event = SendMessage.model_validate(msg)
                     case _:
-                        await ws.send_json(
-                            ActionAck(
-                                type="action.ack",
-                                payload=ActionAckPayload(
-                                    success=False,
-                                    message=f"Unsupported event {msg.get("type", None)}",
-                                ),
-                            ).model_dump()
-                        )
-    except ValidationError:
-        await ws.send_json(
-            ActionAck(
-                type="action.ack",
-                payload=ActionAckPayload(
-                    success=False, message="Invalid event payload"
-                ),
-            ).model_dump()
-        )
+                        raise ValidationError
+                await room_game_managers[room_id].receive_event(
+                    event, uuid_adapters[ws_uuid]
+                )
+
+            except ValidationError:
+                await ws.send_json(
+                    ActionAck(
+                        type="action.ack",
+                        payload=ActionAckPayload(
+                            success=False, message="Invalid event payload"
+                        ),
+                    ).model_dump()
+                )
     except WebSocketDisconnect:
-        if ws in player_map:
-            await manager.broadcast(
-                room_id,
-                PlayerLeft(
-                    type="player.left",
-                    payload=PlayerLeftPayload(player_id=player_map[ws]),
-                ),
-            )
-            del player_map[ws]
-        manager.disconnect(ws, room_id)
+        logging.info(f"Websocket {ws_uuid} disconnected")
+    finally:
+        await room_game_managers[room_id].receive_event(
+            PlayerLeave(
+                type="player.leave",
+                payload=PlayerLeavePayload(player_id=ws_uuid),
+            ),
+            uuid_adapters[ws_uuid],
+        )
+        del uuid_adapters[ws_uuid]
+        if len(room_game_managers[room_id].players) == 0:
+            del room_game_managers[room_id]
